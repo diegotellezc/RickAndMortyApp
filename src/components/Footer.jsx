131a35033b337e92695e82d7f7f744f2bca8f184
Diego Tellez <diegotellezcontreras@gmail.com>
2023-04-27 19:56:09 -0500

    Added hover scale to the icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,21 +3,21 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className='mt-auto flex flex-col items-center text-dark-green m-8 gap-2 md:flex-row md:justify-evenly'>
-            
+
             <div className='flex gap-x-4 md:order-last'>
-                <a className='text-4xl text-dark-green' target='_blank' href="https://github.com/diegotellezc">
+                <a className='text-4xl text-dark-green hover:scale-125' target='_blank' href="https://github.com/diegotellezc">
                     <i className='bx bxl-github' ></i>
                 </a>
 
-                <a className='text-4xl text-dark-green' target='_blank' href="https://www.linkedin.com/in/diegotellezc/">
+                <a className='text-4xl text-dark-green hover:scale-125' target='_blank' href="https://www.linkedin.com/in/diegotellezc/">
                     <i className='bx bxl-linkedin-square' ></i>
                 </a>
 
-                <a className='text-4xl text-dark-green' target='_blank' href="https://www.instagram.com/diegotellezc/">
+                <a className='text-4xl text-dark-green hover:scale-125' target='_blank' href="https://www.instagram.com/diegotellezc/">
                     <i className='bx bxl-instagram-alt' ></i>
                 </a>
 
-                <a className='text-4xl text-dark-green' target='_blank' href="https://tictactoebydt.netlify.app/">
+                <a className='text-4xl text-dark-green hover:scale-125' target='_blank' href="https://tictactoebydt.netlify.app/">
                     <i className='bx bx-question-mark' ></i>
                 </a>
             </div>
